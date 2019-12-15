353c09bd6a3f6a60d869a88ce9b12e9802b6c165
unknown <reihanagam>
2019-12-15 13:07:49 +0700

    update

--- dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Route = express.Router();
-
-const Auth = require('../controllers/auth');
-
-Route.post('/login', Auth.login).post('/register', Auth.register);
-
-module.exports = Route;
