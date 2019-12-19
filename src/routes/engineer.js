00caa6c45b47bf92ac80a9f07aab2fd38e96a773
unknown <reihanagam>
2019-12-19 22:43:02 +0700

    update

--- src/routes/engineer.js
+++ src/routes/engineer.js
@@ -69,9 +69,9 @@ const upload = multer({ storage }).any()
 const Engineer = require('../controllers/engineer')
 
 Route.get('/', Engineer.getAllData)
-  .post('/', auth.check, upload, Engineer.storeData)
+  .post('/', upload, Engineer.storeData)
   .get('/:id', Engineer.editData)
-  .patch('/:id', auth.check, upload, Engineer.updateData)
-  .delete('/:id', auth.check, Engineer.deleteData)
+  .patch('/:id', upload, Engineer.updateData)
+  .delete('/:id',  Engineer.deleteData)
 
 module.exports = Route
