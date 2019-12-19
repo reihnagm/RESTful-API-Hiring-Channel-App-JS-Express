d10d49de28a83b97f32293fdb6aa60c6b95faf2a
unknown <reihanagam>
2019-12-19 21:13:47 +0700

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -27,38 +27,38 @@ module.exports = {
       })
     }
 
-    userModel
-      .login(email)
-      .then(result => {
+    userModel.login(email).then(result => {
+        const id = result[0].id
         const matchedEmail = email === result[0].email
         const matchedPassword = bcrypt.compareSync(password, result[0].password)
         const matchedRoleId = role_id === result[0].role_id
         const login = matchedEmail && matchedPassword && matchedRoleId
 
         if (login) {
-          const token = JWT.sign(
-            {
-              email,
-              role_id
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: '1h'
-            }
-          )
+            const token = JWT.sign(
+                {
+                    id,
+                    email,
+                    role_id
+                },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: '1h'
+                }
+            )
 
-          return res.status(200).json({
-            error: false,
-            status: 200,
-            data: result,
-            message: 'Success login',
-            token
-          })
-        } else {
-          return res.status(400).json({
-            error: true,
-            status: 400,
-            message: 'Email and Passsword not match'
+            return res.status(200).json({
+                error: false,
+                status: 200,
+                data: result,
+                message: 'Success login',
+                token
+            })
+            } else {
+            return res.status(400).json({
+                error: true,
+                status: 400,
+                message: 'Email and Passsword not match'
           })
         }
       })
@@ -75,47 +75,51 @@ module.exports = {
     const password = req.body.password
     const role_id = req.body.role_id
 
+    User.getId(email).then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    })
+
     const validEmail = /[a-zA-Z0-9_]+@[a-zA-Z]+\.(com|net|org)$/.test(email)
 
     if (!validEmail) {
       return res.status(400).json({
         status: 400,
-        error: 'true',
+        error: true,
         message: 'Invalid Email e.g johndoe@gmail.com'
       })
     }
 
     if (!email || !password) {
-      return res.status(400).json({
-        status: 400,
-        error: 'true',
-        message: 'Email and Password required'
-      })
+        return res.status(400).json({
+            status: 400,
+            error: true,
+            message: 'Email and Password required'
+        })
     }
 
     const salt = bcrypt.genSaltSync(10)
     const passwordHash = bcrypt.hashSync(password, salt)
 
     const data = {
-      email,
-      password: passwordHash,
-      role_id
+        email,
+        password: passwordHash,
+        role_id
     }
 
     const token = JWT.sign(
-      {
-        email,
-        role_id
-      },
-      process.env.JWT_KEY,
-      {
-        expiresIn: '1h'
-      }
+        {
+            email,
+            role_id
+        },
+        process.env.JWT_KEY,
+        {
+            expiresIn: '1h'
+        }
     )
 
-    userModel
-      .register(data)
-      .then(result => {
+    userModel.register(data).then(result => {
         return res.status(201).json({
             error: false,
             status: 201,
--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,37 @@
 const conn = require('../configs/db')
 
 module.exports = {
-  login: (email) => {
-    return new Promise((resolve, reject) => {
-      conn.query(`SELECT * FROM user WHERE email = '${email}'`, (err, result) => {
-        if (err) {
-          reject(new Error(err))
-        } else {
-          resolve(result)
-        }
-      })
-    })
-  },
-  register: (data) => {
-    return new Promise((resolve, reject) => {
-      conn.query('INSERT INTO user SET ?', data, (err, result) => {
-        if (err) {
-          reject(new Error(err))
-        } else {
-          resolve(result)
-        }
-      })
-    })
-  }
+    login: (email) => {
+        return new Promise((resolve, reject) => {
+            conn.query(`SELECT * FROM user WHERE email = '${email}'`, (err, result) => {
+                if (err) {
+                    reject(new Error(err))
+                } else {
+                    resolve(result)
+                }
+            })
+        })
+    },
+    register: (data) => {
+        return new Promise((resolve, reject) => {
+            conn.query('INSERT INTO user SET ?', data, (err, result) => {
+                if (err) {
+                    reject(new Error(err))
+                } else {
+                    resolve(result)
+                }
+            })
+        })
+    }.
+    getId: (email) => {
+        return new Promise((resolve, reject) => {
+            conn.query(`SELECT id FROM user WHERE email = ${email}`, (err, result) => {
+                if (err) {
+                    reject(new Error(err))
+                } else {
+                    resolve(result)
+                }
+            })
+        })
+    }
 }
