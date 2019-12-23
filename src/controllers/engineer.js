b75577430baa5b4df069e7751c8fd4037608a0e9
unknown <reihanagam>
2019-12-23 11:49:33 +0700

    update

--- src/controllers/engineer.js
+++ src/controllers/engineer.js
@@ -36,71 +36,70 @@ module.exports = {
             const prevPage = page === 1 ? 1 : page - 1
             const nextPage = page === total[0].total ? total[0].total : page + 1
 
-            let getEngineers = await Engineer.all(offset, limit, sort, sortBy, search)
+            let data = await Engineer.all(offset, limit, sort, sortBy, search)
 
-            response.status(200).json({
-                data: getEngineers,
+            response.json({
+                data,
                 total: Math.ceil(total[0].total),
                 per_page: limit,
                 current_page: page,
                 nextLink: `http://localhost:3001${request.originalUrl.replace('page=' + page, 'page=' + nextPage)}`,
-                prevLink: `http://localhost:3001${request.originalUrl.replace('page=' + page, 'page=' + prevPage)}`,
-                message: 'Success getting all data'
+                prevLink: `http://localhost:3001${request.originalUrl.replace('page=' + page, 'page=' + prevPage)}`
             })
         }
-        catch(error) {
+        catch (error) {
             console.error(error)
             response.status(500).send('Server Error')
         }
 
-          // redis.get(
-          //   `page - ${page} - search ${search} - limit ${limit} - ${sort} - ${sortBy}`,
-          //   (errRedis, resultRedis) => {
-          //     if (errRedis) {
-          //       res.status(400).json({
-          //         error: true,
-          //         message: errRedis
-          //       })
-          //     }
+        // redis.get(
+        //   `page - ${page} - search ${search} - limit ${limit} - ${sort} - ${sortBy}`,
+        //   (errRedis, resultRedis) => {
+        //     if (errRedis) {
+        //       res.status(400).json({
+        //         error: true,
+        //         message: errRedis
+        //       })
+        //     }
 
-          // let pageDetail = {
-          //   total_data: totalDataEngineer,
-          //   per_page: limit,
-          //   current_page: page,
-          //   nextLink: `http://localhost:3000${req.originalUrl.replace('page=' + page, 'page=' + nextPage)}`,
-          //   prevLink: `http://localhost:3000${req.originalUrl.replace('page=' + page, 'page=' + prevPage)}`,
-          // }
+        // let pageDetail = {
+        //   total_data: totalDataEngineer,
+        //   per_page: limit,
+        //   current_page: page,
+        //   nextLink: `http://localhost:3000${req.originalUrl.replace('page=' + page, 'page=' + nextPage)}`,
+        //   prevLink: `http://localhost:3000${req.originalUrl.replace('page=' + page, 'page=' + prevPage)}`,
+        // }
 
-          // if (resultRedis) {
-          //   res.status(200).json({
-          //     status: 200,
-          //     error: false,
-          //     source: 'cache',
-          //     data: JSON.parse(resultRedis),
-          //     total_data: Math.ceil(totalDataEngineer),
-          //     per_page: limit,
-          //     current_page: page,
-          //     nextLink: `http://localhost:3001${req.originalUrl.replace(
-          //       'page=' + page,
-          //       'page=' + nextPage
-          //     )}`,
-          //     prevLink: `http://localhost:3001${req.originalUrl.replace(
-          //       'page=' + page,
-          //       'page=' + prevPage
-          //     )}`,
-          //     message: 'Success getting all data use redis'
-          //   })
-          // } else {
-          // Set Cache Expiration to 1 Hour (60 minutes)
+        // if (resultRedis) {
+        //   res.status(200).json({
+        //     status: 200,
+        //     error: false,
+        //     source: 'cache',
+        //     data: JSON.parse(resultRedis),
+        //     total_data: Math.ceil(totalDataEngineer),
+        //     per_page: limit,
+        //     current_page: page,
+        //     nextLink: `http://localhost:3001${req.originalUrl.replace(
+        //       'page=' + page,
+        //       'page=' + nextPage
+        //     )}`,
+        //     prevLink: `http://localhost:3001${req.originalUrl.replace(
+        //       'page=' + page,
+        //       'page=' + prevPage
+        //     )}`,
+        //     message: 'Success getting all data use redis'
+        //   })
+        // } else {
+        // Set Cache Expiration to 1 Hour (60 minutes)
 
-          // redis.setex(
-          //   `page - ${page} - search ${search} - limit ${limit} - ${sort} - ${sortBy}`,
-          //   3600,
-          //   JSON.stringify(result)
-          // )
+        // redis.setex(
+        //   `page - ${page} - search ${search} - limit ${limit} - ${sort} - ${sortBy}`,
+        //   3600,
+        //   JSON.stringify(result)
+        // )
 
-          // }
-          // })
+        // }
+        // })
         // }).catch(error => {
         //     response.status(400).json({
         //         status: 400,
@@ -108,7 +107,7 @@ module.exports = {
         //         message: error
         //     })
         // })
-      },
+    },
     storeData: async (request, response) => {
 
         // if(request.files >= 5242880)  { // 5 MB
@@ -191,60 +190,60 @@ module.exports = {
         //     })
         // })
 
-    // request.checkBody('avatar', 'Avatar - Please upload an image JPG, JPEG, PNG or GIF').isImage(typeof request.files['avatar'] !== "undefined" ? request.files['avatar'][0].filename : '');
+        // request.checkBody('avatar', 'Avatar - Please upload an image JPG, JPEG, PNG or GIF').isImage(typeof request.files['avatar'] !== "undefined" ? request.files['avatar'][0].filename : '');
 
-    // const showcase =  req.files[0].originalname
-    // const avatar =  req.files[1].originalname
+        // const showcase =  req.files[0].originalname
+        // const avatar =  req.files[1].originalname
 
-    // if (req.files[0].size >= 5242880) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Showcase size cannot larger than 5MB'
-    //   })
-    // }
-    //
-    // if (req.files[1].size >= 5242880) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Avatar size cannot larger than 5MB'
-    //   })
-    // }
-    //
-    // if (!checkext.checkFileImg(req.files[0].mimetype)) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
-    //   })
-    // }
-    //
-    // if (!checkext.checkFileImg(req.files[1].mimetype)) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
-    //   })
-    // }
-    //
-    // if (req.fileValidationError) {
-    //   res.status(400).json({
-    //     status: '400',
-    //     message: req.fileValidationError
-    //   })
-    // }
+        // if (req.files[0].size >= 5242880) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Showcase size cannot larger than 5MB'
+        //   })
+        // }
+        //
+        // if (req.files[1].size >= 5242880) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Avatar size cannot larger than 5MB'
+        //   })
+        // }
+        //
+        // if (!checkext.checkFileImg(req.files[0].mimetype)) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
+        //   })
+        // }
+        //
+        // if (!checkext.checkFileImg(req.files[1].mimetype)) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
+        //   })
+        // }
+        //
+        // if (req.fileValidationError) {
+        //   res.status(400).json({
+        //     status: '400',
+        //     message: req.fileValidationError
+        //   })
+        // }
 
 
     },
     updateData: (request, response) => {
 
-        if(request.files >= 5242880)  { // 5 MB
-            return response.status(400).json({ status: 400, error: true, message: "File too large"})
+        if (request.files >= 5242880) { // 5 MB
+            return response.status(400).json({ status: 400, error: true, message: "File too large" })
         }
 
         if (!request.files) {
-            return response.status(400).json({ status: 400, error: true, message: "Please upload file"})
+            return response.status(400).json({ status: 400, error: true, message: "Please upload file" })
         }
 
         if (!validationResult(request).isEmpty()) {
@@ -291,47 +290,47 @@ module.exports = {
             })
         })
 
-    // const showcase = req.files[0].originalname
-    // const avatar = req.files[1].originalname
+        // const showcase = req.files[0].originalname
+        // const avatar = req.files[1].originalname
 
-    // if (req.files[0].size >= 5242880) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Showcase size cannot larger than 5MB'
-    //   })
-    // }
-    //
-    // if (req.files[1].size >= 5242880) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Avatar size cannot larger than 5MB'
-    //   })
-    // }
-    //
-    // if (!checkext.checkFileImg(req.files[0].mimetype)) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
-    //   })
-    // }
-    //
-    // if (!checkext.checkFileImg(req.files[1].mimetype)) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: true,
-    //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
-    //   })
-    // }
-    //
-    // if (req.fileValidationError) {
-    //   res.status(400).json({
-    //     status: '400',
-    //     message: req.fileValidationError
-    //   })
-    // }
+        // if (req.files[0].size >= 5242880) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Showcase size cannot larger than 5MB'
+        //   })
+        // }
+        //
+        // if (req.files[1].size >= 5242880) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Avatar size cannot larger than 5MB'
+        //   })
+        // }
+        //
+        // if (!checkext.checkFileImg(req.files[0].mimetype)) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
+        //   })
+        // }
+        //
+        // if (!checkext.checkFileImg(req.files[1].mimetype)) {
+        //   return res.status(400).json({
+        //     status: 400,
+        //     error: true,
+        //     message: 'Support type file only : JPEG, GIF, PNG, SVG, BMP'
+        //   })
+        // }
+        //
+        // if (req.fileValidationError) {
+        //   res.status(400).json({
+        //     status: '400',
+        //     message: req.fileValidationError
+        //   })
+        // }
 
     },
     editData: (request, response) => {
@@ -364,9 +363,9 @@ module.exports = {
             })
         }).catch(error => {
             response.status(400).json({
-              status: 400,
-              error: true,
-              message: error
+                status: 400,
+                error: true,
+                message: error
             })
         })
     }
