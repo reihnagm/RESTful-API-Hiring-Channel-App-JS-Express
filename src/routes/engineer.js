91a877db91cea236fc4c93e2c19d71936336f0de
unknown <reihanagam>
2019-12-29 14:54:30 +0700

    update

--- src/routes/engineer.js
+++ src/routes/engineer.js
@@ -22,21 +22,17 @@ const Engineer = require('../controllers/engineer')
 
 Route.get('/', Engineer.getAllData)
     .post('/', [
-        check('name', 'Name is required').trim().not().isEmpty(),
         check('description', 'Description is required').trim().not().isEmpty(),
         check('skill', 'Skill is required').trim().not().isEmpty(),
         check('location', 'Location is required').trim().not().isEmpty(),
-        check('email', 'Please include valid email').trim().isEmail().normalizeEmail(),
         check('telephone', 'Telephone is required').trim().not().isEmpty(),
         check('salary', 'Salary is required').trim().not().isEmpty()
     ], Engineer.storeData)
     .get('/:id', Engineer.editData)
     .patch('/:id', [
-        check('name', 'Name is required').trim().not().isEmpty(),
         check('description', 'Description is required').trim().not().isEmpty(),
         check('skill', 'Skill is required').trim().not().isEmpty(),
         check('location', 'Location is required').trim().not().isEmpty(),
-        check('email', 'Please include valid email').trim().not().isEmpty().isEmail().normalizeEmail(),
         check('telephone', 'Telephone is required').trim().not().isEmpty(),
         check('salary', 'Salary is required').trim().not().isEmpty()
     ], Engineer.updateData)
