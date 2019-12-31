4214a7c08ae26a3aa122e70d5143d7a409417848
unknown <reihanagam>
2019-12-31 09:22:36 +0700

    update

--- src/models/Engineer.js
+++ src/models/Engineer.js
@@ -17,7 +17,7 @@ module.exports = {
     all: (offset, limit, sort, sortBy, search) => {
         return new Promise((resolve, reject) => {
             const query = `SELECT a.*, b.name, b.email FROM engineer a INNER JOIN user b ON a.user_id = b.id
-            WHERE (location LIKE '%${search}%' or skill LIKE '%${search}%')
+            WHERE (name LIKE '%${search}%' or skill LIKE '%${search}%')
             ORDER BY ${sortBy} ${sort} LIMIT ${offset}, ${limit}`
             connection.query(query, (error, result) => {
                 if (error) {
